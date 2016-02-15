ee9cb76a7e409852300ffa834356a7817f3e1704
Vitaliano <vitaliano.neto@gmail.com>
2016-02-15 18:23:50 -0200

    Renamed file Included the returning statement on comments Function now returns the same string written on file

--- datastore-exporter.js
+++ datastore-exporter.js
@@ -3,6 +3,7 @@
 var moment = require("moment-timezone");
 var path = require("path");
 var fs = require("fs");
+var logFileNameFormat = "YYYYMMDD[.log]";
 
 /*
  * Exports the data from data_array to datastore format using the hierarchy and datetime to determine the file that will be written
@@ -12,10 +13,12 @@ var fs = require("fs");
  * @param {string} tagname - sets the name that will be used as the entry tag
  * @param {array} data_array - sets the data that will be written on file, it will be converted to Number before its written
  * @param {string} str_commentary - sets the commentary of the data written in the format "color r:%f g:%f b:%f" it accepts %d and %f only
+ * 
+ * @returns {string} - the same string written to the file
  */
 exports.writeToDatastoreLog = function (hierarchy, datetime, tagname, data_array, str_commentary) {
     if(datetime && tagname && data_array) {
-        unix_ms = 0;
+        var unix_ms = 0;
         if(datetime instanceof moment) {
             unix_ms = datetime.valueOf();
         }
@@ -27,7 +30,7 @@ exports.writeToDatastoreLog = function (hierarchy, datetime, tagname, data_array
             datetime = new moment.tz(new Date(datetime));
             unix_ms = datetime.valueOf();
         }
-        var dir = pathjs.dirname(hierarchy);
+        var dir = path.normalize(hierarchy);
         makeDir(dir);
         var data_str = [];
         data_array.forEach(function(item){
@@ -37,26 +40,27 @@ exports.writeToDatastoreLog = function (hierarchy, datetime, tagname, data_array
         });
         var dataLog = `${unix_ms}, 2, ${data_array.length}, LOG, ${tagname}, ${data_str.join()}, ${str_commentary} \r\n`;
         fs.appendFileSync(path.join(hierarchy, datetime.clone().utc().format(logFileNameFormat)), dataLog);
+        return dataLog;
     } else {
         throw new Error("The parameter " + (datetime ? (tagname ? "data_array" : "tagname") : "datetime") + " is missing");
     }
-}
+};
 
 function makeDir(str_path) {
     try {
         fs.mkdirSync(str_path);
     } catch(ex) {
         // console.log(JSON.stringify(ex));
-        if(ex.code != "EEXIST") {
-            if(ex.code == "ENOENT") {
-                var dirPathSplit = str_path.split(pathjs.sep);
+        if(ex.code !== "EEXIST") {
+            if(ex.code === "ENOENT") {
+                var dirPathSplit = str_path.split(path.sep);
                 var dirPath = "";
                 dirPathSplit.forEach(function (value) {
-                    dirPath = pathjs.join(dirPath, value);
+                    dirPath = path.join(dirPath, value);
                     makeDir(dirPath);
                 });
             } else {
-                console.log(JSON.stringify(ex));
+                // console.log(JSON.stringify(ex));
                 throw ex;
             }
         }
